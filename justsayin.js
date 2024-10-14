d24f38bb477bb3e272ac2b0ada843fc53f0cd1f4
Aidan Perkins <aidan.perkins@velocigo.com>
2024-10-14 13:55:27 -0500

    Added account login and signup pages

--- justsayin.js
+++ justsayin.js
@@ -1,4 +1,6 @@
 const toggleBtn = document.getElementById("resume-toggle-button");
+const loginBtn = document.getElementById("login-button");
+const signupBtn = document.getElementById("signup-button");
 const resume = document.getElementById("resume-pdf");
 
 toggleBtn.addEventListener("click", () => {
@@ -11,3 +13,11 @@ toggleBtn.addEventListener("click", () => {
         toggleBtn.textContent = "Hide Resume"
     }
 });
+
+loginBtn.addEventListener("click", () => {
+    window.location.href = "login.html"
+})
+
+signupBtn.addEventListener("click", () => {
+    window.location.href = "signup.html"
+})
